f59c8da202829463c250f7b180f339e5e7227b8f
Alejandro Jordan <ajordanbojanic@gmail.com>
2022-09-27 20:40:29 -0500

    comments

--- useful_tools/data.py
+++ useful_tools/data.py
@@ -6,7 +6,10 @@ class Data():
         self.file_name = file_name
         self.output_name = output_name
 
-    def remove_ouliers_using_std(df, columns=list, n_std=float):
+    def remove_ouliers_using_std(df, columns=list, n_std=3):
+        '''Recommended - assumes Gaussian distribution 
+           Works for an array of columns, industry best practice is n_std = 3 
+        '''
         for col in columns:
             mean = df[col].mean()
             sd = df[col].std()
@@ -14,6 +17,10 @@ class Data():
         return df
 
     def remove_outliers_using_quantiles(df, col_name, upper=0.95, lower=0.05):
+        '''Simplified - does not assume/require Gaussian distribution
+            Works for a single colum from which the user needs to remove outliers 
+            Can be extended to multiple columns
+        '''
         lower = df[f'{col_name}'].quantile(lower)
         upper = df[f'{col_name}'].quantile(upper)
         return df.clip(lower=lower, upper=upper)
