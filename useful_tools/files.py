c8489bad448ef002b87f71052a09c808c5a7728e
Alejandro Jordan <ajordanbojanic@gmail.com>
2022-09-28 21:49:03 -0500

    add merge csv files

--- useful_tools/files.py
+++ useful_tools/files.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from datetime import datetime
-
+import os 
 
 class File():
-    def __init__(self, file_name=str, output_name=str, test_dir=str):
+    def __init__(self, file_name=str, output_name=str, test_dir=str, data_dir=str):
         self.file_name = file_name
         self.output_name = output_name
         self.test_dir = test_dir
+        self.data_directory = data_dir
 
     def json_to_csv(self):
         df = pd.read_json(self.file_name)
@@ -30,3 +31,13 @@ class File():
         return df.to_json(
             f"{self.test_dir}/{self.output_name}-{datetime.utcnow().strftime('%Y-%m-%d-%H-%M')}.json"
         )
+    def merge_csv_files(self):
+        '''Assumes you have created both directories, the data and the one you will use to save these new files
+        Aassumes the csv files have the same column definitions. '''
+        files = [file for file in os.listdir(f'./{self.data_directory}')]
+        df = pd.DataFrame()
+        for file in files:
+            df_csv = pd.read_csv(f'./{self.data_directory}/'+file)
+            input = pd.concat([df,df_csv])
+            input.to_csv(f"{self.test_dir}/{self.output_name}-{datetime.utcnow().strftime('%Y-%m-%d-%H-%M')}.csv")
+        return input
