a584954f535c96990ca1bb72b2246a1086a8a81b
Alejandro Jordan <ajordanbojanic@gmail.com>
2022-09-28 21:59:58 -0500

    update data methods, extend quantiles method to multiple columns

--- useful_tools/data.py
+++ useful_tools/data.py
@@ -6,21 +6,26 @@ class Data():
         self.file_name = file_name
         self.output_name = output_name
 
-    def remove_ouliers_using_std(df, columns=list, n_std=3):
+    def remove_ouliers_using_std(dt, columns=list, n_std=3):
         '''Recommended - assumes Gaussian distribution 
            Works for an array of columns, industry best practice is n_std = 3 
+           https://www.geeksforgeeks.org/how-to-get-column-names-in-pandas-dataframe/
         '''
+        df = pd.DataFrame(dt)
         for col in columns:
             mean = df[col].mean()
             sd = df[col].std()
             df = df[(df[col] <= mean+(n_std*sd))]
         return df
 
-    def remove_outliers_using_quantiles(df, col_name, upper=0.95, lower=0.05):
+    def remove_outliers_using_quantiles(dt, columns=list, upper=0.95, lower=0.05):
         '''Simplified - does not assume/require Gaussian distribution
-            Works for a single colum from which the user needs to remove outliers 
-            Can be extended to multiple columns
+            Works for an array of columns, lower and upper bounds change depending on the tolerance
+            https://www.geeksforgeeks.org/how-to-get-column-names-in-pandas-dataframe/
         '''
-        lower = df[f'{col_name}'].quantile(lower)
-        upper = df[f'{col_name}'].quantile(upper)
-        return df.clip(lower=lower, upper=upper)
+        df = pd.DataFrame(dt)
+        for col in columns:
+            lower = df[f'{col}'].quantile(lower)
+            upper = df[f'{col}'].quantile(upper)
+            df.clip(lower=lower, upper=upper)
+        return df
